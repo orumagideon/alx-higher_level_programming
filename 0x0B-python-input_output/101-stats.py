6fedfa93ba1ec41396d948b1517e2e15bada382d
orumagideon <orumagideon535@gmail.com>
2023-09-12 08:00:56 -0700

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,47 +1,29 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+
 import sys
-import signal
+import collections
 
-"""Initialize variables to store metrics"""
-total_file_size = 0
-status_code_counts = {}
+status_code_counts = collections.defaultdict(int)
+total_size = 0
 
-""" Define a function to handle SIGINT (CTRL + C) and print metrics"""
-def handle_interrupt(sig, frame):
-    print_metrics()
-
-"""Register the signal handler for SIGINT"""
-signal.signal(signal.SIGINT, handle_interrupt)
-
-"""Function to print metrics"""
-def print_metrics():
-    print(f"File size: {total_file_size}")
-    sorted_status_codes = sorted(status_code_counts.keys())
-    for code in sorted_status_codes:
-        count = status_code_counts[code]
-        print(f"{code}: {count}")
-
-line_count = 0
-
-"""Read input lines from stdin"""
 for line in sys.stdin:
-    line_count += 1
-    parts = line.split()
-    
-    """ Check if the line has the expected format"""
-    if len(parts) >= 7:
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-        
-        """ Update total file size"""
-        total_file_size += file_size
-        
-        """Update status code counts"""
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-        else:
-            status_code_counts[status_code] = 1
-        
-        """ Print metrics every 10 lines"""
-        if line_count % 10 == 0:
-            print_metrics()
+
+    line_data = line.split()
+
+    status_code = int(line_data[3])
+
+    status_code_counts[status_code] +=1
+    total_size += int(line_data[4])
+
+    if len(status_code_counts) % 10 == 0:
+        print("File size:", total_size)
+        for status_code, count in status_code_counts.items():
+            print(status_code, ":", count)
+
+try:
+    while True:
+        print("File size:", total_size)
+        for status_code, count in status_code_counts.items():
+            print(status_code, ":", count)
+except KeyboardInterrupt:
+    pass
