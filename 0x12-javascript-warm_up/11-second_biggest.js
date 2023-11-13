0bcf56de0772055a6966c4032bcf0e36d1b07877
orumagideon <orumagideon535@gmail.com>
2023-11-13 01:18:43 -0800

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).map(arg => parseInt(arg));
+const args = process.argv.slice(2).map(Number);
 
 if (args.length <= 1) {
   console.log(0);
 } else {
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
