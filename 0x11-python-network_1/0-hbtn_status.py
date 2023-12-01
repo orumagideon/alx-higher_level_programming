5f6815ebbeab9a4e25ccc212ecc691145b990662
orumagideon <orumagideon535@gmail.com>
2023-12-01 03:46:48 -0800

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-
+"""
+ Fetches a url and displays the response
+"""
 import urllib.request
-import urllib.error
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    try:
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(html)))
-            print("\t- content: {}".format(html))
-            print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("HTTPError: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print("URLError: {}".format(e.reason))
+    r = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(r) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
